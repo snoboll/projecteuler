0fb3e128ceb7f25088ba5202906bbccfbbf08096
Jesper Laurell <jesper.laurell1@gmail.com>
2019-06-04 23:49:56 +0200

    euler 18 done

--- euler18.py
+++ euler18.py
@@ -1,9 +1,20 @@
 #euler 18
 f = open("euler18input.txt", "r")
-a = []
+
+tree = [[0] for i in range(15)]
+
+for i in range(13,-1,-1):
+	print(i)
+
 for i in range(15):
 	f1 = f.readline().split()
 	f2 = [int(i) for i in f1]
-	a.append(f1)
+	tree[i] = f2
 
-print(a)
+ans = 0
+for i in range(13,-1,-1):
+	for j in range(i+1):
+		tree[i][j] += max(tree[i+1][j], tree[i+1][j+1])
+
+print(tree)
+print(tree[0][0])
