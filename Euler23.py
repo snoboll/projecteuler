5e7534326d98b90f9799b5b12bda6556b3de6b37
snoboll <jesper.laurell1@gmail.com>
2019-06-24 21:29:36 +0200

    showing janna coool stuff

--- Euler23.py
+++ Euler23.py
@@ -1,27 +1,47 @@
 #Euler23
 import time
 start_time = time.time()
+cap = 100
+
+#checking abundant numbers
 def isAbund(n):
     divs = set()
     divsum = sum([div for div in range(1,n) if n%div == 0])
     return(divsum>n)
 
-cap1 = 28123
 abund = []
-for i in range(cap1):
+for i in range(cap):
     if isAbund(i):
         abund.append(i)
 
-'''
-for n in range(20):
-    validnr = True
-    for i in abund:
-        for j in abund:
+#binary search
+def binsearch(list, item):
+    hi = len(list)-1
+    lo = 0
+    found = False
+    while lo<=hi and not found:
+        mid = (hi+lo)//2
+        if list[mid] == item:
+            found = True
+        else:
+            if item < list[mid]:
+                hi = mid-1
+            else:
+                lo = mid+1
+    return True
+
+
+#getting nums which cant be sum omf abund numbers
+testcap = 100
+for n in range(testcap):
+    for i in range(n):
+        a = abund[i]
+        for j in range(n):
             if n == j+i:
                 validnr = False
                 break
     if validnr:
         sum+=n
-'''
+
 print(abund)
 print("--- %s seconds ---" % (time.time()-start_time))
--- euler14.py
+++ euler14.py
@@ -2,7 +2,8 @@ with open('euler14input.txt', 'r') as f:
 	f_contents = f.readlines()
 	print(f_contents)
 
-		
-	
+
+
 	pass
-	
+
+#adding comment
